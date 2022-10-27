3402faf77961452f43d415018ebead730aacba7f
Bookcliff <bookcliff76@gmail.com>
2022-10-27 15:14:16 -0500

    add element to DOM

--- js/tvl.js
+++ js/tvl.js
@@ -17,39 +17,32 @@ bondGraph("https://api.thegraph.com/subgraphs/name/alwaysbegrowing/arbor-v1", {
 }).then(async (data) => {
   let collateral = 0;
   //Loop through each bond to calculate the collateral amount for each bond individually
-  const getTokenPrice = async () => {
-    data.data.bonds.forEach(async (element) => {
-      //Contract address of the collateral token used
-      const collateralToken = element.collateralToken.id;
-      //Amount of tokens of the collateral token
-      const collateralAmount = element.collateralTokenAmount;
-      //Number of decimals for collateral token
-      const decimals = element.collateralToken.decimals;
-      //Correct the collateral amount based on the number of decimals
-      const updatedCollateralAmount = collateralAmount / 10 ** decimals;
-      //Make GET request to CoinGecko to get the current price of the collateral token
-      async function tokenPrice() {
-        await fetch(
-          `https://api.coingecko.com/api/v3/simple/token_price/ethereum?vs_currencies=usd&contract_addresses=${collateralToken}`
-        )
-          .then((response) => response.json())
-          //Pull out the price in USD
-          .then((data) => {
-            const usd = data?.[collateralToken].usd;
-            //Price of the total collateral for one bond
-            const collateralUsd = usd * updatedCollateralAmount;
-            //Increment the total collateral by the current bond collateral amount
-            // console.log({ collateralUsd });
-            return collateralUsd;
-          });
-      }
-      let collateralTotal = await tokenPrice();
-      console.log({ collateralTotal });
-    });
+  const getTokenPrice = async (element) => {
+    //Contract address of the collateral token used
+    const collateralToken = element.collateralToken.id;
+    //Amount of tokens of the collateral token
+    const collateralAmount = element.collateralTokenAmount;
+    //Number of decimals for collateral token
+    const decimals = element.collateralToken.decimals;
+    //Correct the collateral amount based on the number of decimals
+    const updatedCollateralAmount = collateralAmount / 10 ** decimals;
+    //Make GET request to CoinGecko to get the current price of the collateral token
+    const tokenPrice = await fetch(
+      `https://api.coingecko.com/api/v3/simple/token_price/ethereum?vs_currencies=usd&contract_addresses=${collateralToken}`
+    );
+    const tokenPriceResponse = await tokenPrice.json();
+    //USD for 1 collateral token
+    const usd = tokenPriceResponse?.[collateralToken].usd;
+    //Price of the total collateral for one bond
+    const collateralUsd = usd * updatedCollateralAmount;
+    collateral += collateralUsd;
+
+    return collateral;
   };
   for (let i = 0; i < data.data.bonds.length; i++) {
-    collateral += await getTokenPrice();
-    // console.log(collateral);
+    collateral += await getTokenPrice(data.data.bonds[i]);
   }
-  console.log(collateral);
+  //Add element to DOM
+  const ddUpdate = document.getElementById("collateral");
+  ddUpdate.appendChild(document.createTextNode(collateral));
 });
