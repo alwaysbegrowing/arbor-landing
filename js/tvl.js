c9e8a677dce932e79e9b66e38936924bfb0f1fdc
Bookcliff <bookcliff76@gmail.com>
2022-10-27 17:03:40 -0500

    fixed reviews

--- js/tvl.js
+++ js/tvl.js
@@ -1,3 +1,6 @@
+const SUBGRAPH_URL =
+  "https://api.thegraph.com/subgraphs/name/alwaysbegrowing/arbor-v1";
+
 //Make POST request to GraphQL to obtain collateral token info
 async function bondGraph(url = "", data = {}) {
   const response = await fetch(url, {
@@ -11,9 +14,17 @@ async function bondGraph(url = "", data = {}) {
 }
 
 //GraphQL query
-bondGraph("https://api.thegraph.com/subgraphs/name/alwaysbegrowing/arbor-v1", {
-  query:
-    "{\n  bonds {\n    id\n    collateralToken {\n      id\n      decimals\n    }\n    collateralTokenAmount\n  }\n}",
+bondGraph(SUBGRAPH_URL, {
+  query: `{
+    bonds {
+      id
+      collateralToken {
+        id
+        decimals
+      }
+      collateralTokenAmount
+    }
+  }`,
 }).then(async (data) => {
   let collateral = 0;
   //Loop through each bond to calculate the collateral amount for each bond individually
@@ -27,24 +38,20 @@ bondGraph("https://api.thegraph.com/subgraphs/name/alwaysbegrowing/arbor-v1", {
     //Correct the collateral amount based on the number of decimals
     const updatedCollateralAmount = collateralAmount / 10 ** decimals;
     //Make GET request to CoinGecko to get the current price of the collateral token
-    const tokenPrice = await fetch(
-      `https://api.coingecko.com/api/v3/simple/token_price/ethereum?vs_currencies=usd&contract_addresses=${collateralToken}`
-    );
+    const COINGECKO_TOKEN_PRICE_URL = `https://api.coingecko.com/api/v3/simple/token_price/ethereum?vs_currencies=usd&contract_addresses=${collateralToken}`;
+    const tokenPrice = await fetch(COINGECKO_TOKEN_PRICE_URL);
     const tokenPriceResponse = await tokenPrice.json();
     //USD for 1 collateral token
     const usd = tokenPriceResponse?.[collateralToken].usd;
     //Price of the total collateral for one bond
     const collateralUsd = usd * updatedCollateralAmount;
-    collateral += collateralUsd;
 
-    return collateral;
+    return collateralUsd;
   };
   for (let i = 0; i < data.data.bonds.length; i++) {
     collateral += await getTokenPrice(data.data.bonds[i]);
   }
   //Add element to DOM
-  const ddUpdate = document.getElementById("collateral");
-  ddUpdate.appendChild(
-    document.createTextNode("$" + collateral.toLocaleString())
-  );
+  const tvl = document.getElementById("tvl");
+  tvl.innerHTML = "$" + collateral.toLocaleString();
 });
